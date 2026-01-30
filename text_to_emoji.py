ef6a946720eb3cebf2d95c55f8b5078f39ff2f11
Shrankha Singh <shrankha.singh03@gmail.com>
2026-01-30 17:12:02 +0530

    Add files via upload
    This beginner-friendly Python mini project focuses on converting simple text into emojis using string manipulation. It helps learners understand the working of the .replace() method, user input handling, and basic program flow, making it an ideal starter project for Python programming.

---
+++ text_to_emoji.py
@@ -0,0 +1,15 @@
+# FIRST MINI PROJECT - Basic Version
+
+# EMOJI CONVERTOR😊
+
+msg= input("Enter your msg:")
+
+msg= (msg.replace(":)","😊"))
+msg= (msg.replace(":(","☹️"))
+msg= (msg.replace(":D","😀"))
+msg= (msg.replace(":;","😉"))
+
+print(msg)
+
+
+
